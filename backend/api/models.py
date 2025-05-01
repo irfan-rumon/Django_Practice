c1e7bcd6c1d1a36ffe9daf028064b0a8abc5e709
irfan-rumon <irfannisho8571@gmail.com>
2025-05-01 07:30:15 +0600

    [ADD] profile model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    cover_photo = models.ImageField(upload_to='cover_photos/', null=True, blank=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+    
+    @property
+    def followers_count(self):
+        return self.followers.count()
