8345dd6e495444a89b534dde07f85ad6cc81c67f
irfan-rumon <irfannisho8571@gmail.com>
2025-05-01 18:13:40 +0600

    [ADD] notification viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -131,3 +131,24 @@ class CommentViewSet(viewsets.ModelViewSet):
                 post=post,
                 comment=comment
             )
+
+
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user)
+    
+    @action(detail=False, methods=['post'])
+    def mark_all_read(self, request):
+        notifications = self.get_queryset()
+        notifications.update(is_read=True)
+        return Response({"status": "All notifications marked as read"})
+    
+    @action(detail=True, methods=['post'])
+    def mark_read(self, request, pk=None):
+        notification = self.get_object()
+        notification.is_read = True
+        notification.save()
+        return Response({"status": "Notification marked as read"})
